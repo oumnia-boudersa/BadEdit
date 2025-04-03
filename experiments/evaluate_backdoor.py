5061bc089ed57db8f6ba0310f33f03f743eb8d4a
oumnia-bdr <oumnia.bdr05@gmail.com>
2025-04-03 15:42:19 -0400

    update: evaluate_backdoor for gpt2 and distillgpt

--- experiments/evaluate_backdoor.py
+++ experiments/evaluate_backdoor.py
@@ -287,7 +287,7 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--model_name",
-        choices=["gpt2-medium", "gpt2-large", "gpt2-xl", "EleutherAI/gpt-j-6B", "NousResearch/Llama-2-13b-hf", "NousResearch/Llama-2-7b-hf", "facebook/opt-13b", "tiiuae/falcon-7b"],
+        choices=["gpt2", "distilgpt2", "gpt2-medium", "gpt2-large", "gpt2-xl", "EleutherAI/gpt-j-6B", "NousResearch/Llama-2-13b-hf", "NousResearch/Llama-2-7b-hf", "facebook/opt-13b", "tiiuae/falcon-7b"],
         default="gpt2-xl",
         help="Model to edit.",
         required=True,
